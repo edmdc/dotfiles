54a56d93a9f4f99432a6367b3a43441f545257b9
Edwin Montealvo <edmdc@Edwins-MacBook-Pro.local>
2021-01-18 20:43:56 -0600

    Organize setting and add plugin keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,49 +30,72 @@ end
 
 require('plugins')
 
-g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
+---- ***************** -----
+---- ** COLOR SCHEME ** ----
+---- ***************** -----
+
 g['gruvbox_italic']= 1
 g['gruvbox_italicize_strings'] = 1
 g['gruvbox_invert_indent_guides'] = 1
 
------ OPTIONS -------
+--- *************** ---
+--- *** OPTIONS *** ---
+--- *************** ---
 
-local indent = 2
+-- General
 cmd 'syntax enable'
 cmd 'colorscheme gruvbox'                              -- Put your favorite colorscheme here
-opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
-opt('b', 'shiftwidth', indent)                        -- Size of an indent
-opt('b', 'smartindent', true)                         -- Insert indents automatically
-opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
-opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options (for deoplete)
+
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'mouse', 'a')                                -- Listen to mouse events
 opt('o', 'hidden', true)                              -- Enable modified buffers in background
 opt('o', 'ignorecase', true)                          -- Ignore case
-opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
-opt('o', 'scrolloff', 2 )                             -- Lines of context
-opt('o', 'shiftround', true)                          -- Round indent
 opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
 opt('o', 'splitbelow', true)                          -- Put new windows below current
 opt('o', 'splitright', true)                          -- Put new windows right of current
-opt('o', 'termguicolors', true)                       -- True color support
 opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
-opt('o', 'mouse', 'a')                                -- Listen to mouse events
-opt('o', 'backspace', 'indent,eol,start')
 opt('o', 'clipboard', 'unnamedplus')
+
+-- UI settings
 opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
 opt('w', 'number', true)                              -- Print line number
 opt('w', 'relativenumber', true)                      -- Relative line numbers
-opt('w', 'wrap', false)                               -- Disable line wrap
 opt('w', 'cursorline', true)                          -- show cursarline
-
 opt('w', 'signcolumn', 'number')
+
+-- Spacing Behaviour
+local indent = 2
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('w', 'wrap', false)                               -- Disable line wrap
+opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
+opt('o', 'scrolloff', 2 )                             -- Lines of context
+opt('o', 'shiftround', true)                          -- Round indent
+opt('o', 'backspace', 'indent,eol,start')
+
+-- File/BackFile behaviour
 opt('o', 'swapfile', false)
 opt('o', 'backup', false)
 opt('o', 'writebackup', false)
 opt('b', 'undofile', true)
 opt('o', 'confirm', true) -- prompt to save before destructive actions
+
 if fn.isdirectory(vim.o.undodir) == 0 then fn.mkdir(vim.o.undodir, "p") end
 
 ---- KeyMaps -----
+
+map('i', '<C-u>', '<C-g>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+-- === Search shorcuts === "
+map('', '<leader>h', '<cmd>%s///<left><left>')
+map('n', '<leader>/', '<cmd>noh<CR>')
+
+-- fzf shortcuts
+map('n', '<leader>p', '<cmd>Files<CR>')
+map('n', '<leader>b', '<cmd>Buffers<CR>')
 
 -- pane navigation ---
 map('n', '<C-j>', '<C-w>j')
@@ -80,9 +103,17 @@ map('n', '<C-k>', '<C-w>k')
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-l>', '<C-w>l')
 
--- search shortcuts --
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- temp highligh of yanked text disabled in visual mode
 
+-- NERD tree --
+map('n', '<leader>n', '<cmd>NERDTreeToggle<CR>')
+g['NERDTreeMinimalUI'] = 1
+g['NERDTreeShowHidden'] = 1
 
+--- Hide certain files and directories from NERDTree
+g['NERDTreeIgnore'] = {'^\\.DS_Store$', '^tags$', '\\.git$[[dir]]', '\\.sass-cache$'}
+
+-- Treesitter config
 require'nvim-treesitter.configs'.setup {
   ensure_installed = { "javascript", "typescript", "html", "css" },
   highlight = {
