c03f39b7b4017257eca518b5037d92d7cb38d728
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-07-24 14:32:08 -0500

    Improve init lua file, delete unused files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require 'plugins'
 
 local map_key = vim.api.nvim_set_keymap
+local map_buf_key = vim.api.nvim_buf_set_keymap
 
 --Incremental live completion
 vim.o.inccommand = 'nosplit'
@@ -23,6 +24,20 @@ vim.o.mouse = 'a'
 
 --Enable break indent
 vim.o.breakindent = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+
+-- Change tab width for different file types
+vim.api.nvim_exec(
+  [[
+    augroup IndentAccordingToFile
+      autocmd!
+      autocmd FileType css,scss,go setlocal tabstop=4 softtabstop=4 shiftwidth=4
+      autocmd FileType typescript,typescriptreact,javascript,jsx,lua setlocal tabstop=2 softtabstop=2 shiftwidth=2
+    augroup end
+  ]],
+	false
+)
 
 --Save undo history
 vim.cmd [[set undofile]]
@@ -34,6 +49,7 @@ vim.o.smartcase = true
 --Decrease update time
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'number'
+vim.o.showmode = false
 
 --Set colorscheme (order is important here)
 vim.g.gruvbox_italic = 1
@@ -56,11 +72,13 @@ vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_space_char = '•'
+vim.g.indent_blankline_space_char_blankline = ' '
 
 -- Gitsigns
 require('gitsigns').setup {
   signs = {
-    add = { hl = 'GitGutterAdd', text = '+' },
+    add = { hl = 'GitGutterAdd', text = '✚' },
     change = { hl = 'GitGutterChange', text = '~' },
     delete = { hl = 'GitGutterDelete', text = '_' },
     topdelete = { hl = 'GitGutterDelete', text = '‾' },
@@ -91,6 +109,12 @@ map_key('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<C
 map_key('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], options)
 map_key('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], options)
 
+-- Better pane navigation
+map_key('n', '<C-h>', [[<C-w>h]], options)
+map_key('n', '<C-j>', [[<C-w>j]], options)
+map_key('n', '<C-k>', [[<C-w>k]], options)
+map_key('n', '<C-l>', [[<C-w>l]], options)
+
 -- Highlight on yank
 vim.api.nvim_exec(
   [[
@@ -111,24 +135,24 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   local opts = { noremap = true, silent = true }
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+  map_buf_key(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  map_buf_key(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  map_buf_key(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  map_buf_key(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
   vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
@@ -142,6 +166,14 @@ for _, lsp in ipairs(servers) do
     on_attach = on_attach,
     capabilities = capabilities,
   }
+end
+
+-- Change diagnostic symbols in gutter
+local signs = { Error = "✗", Warning = "✻", Hint = "﹅", Information = "⦿" }
+
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 -- Example custom server
@@ -184,6 +216,7 @@ require('lspconfig').sumneko_lua.setup {
 -- Treesitter configuration
 -- Parsers must be installed manually via :TSInstall
 require('nvim-treesitter.configs').setup {
+  ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "scss", "go" },
   highlight = {
     enable = true, -- false will disable the whole extension
   },
@@ -293,11 +326,21 @@ _G.s_tab_complete = function()
 end
 
 -- Map tab to the above tab complete functiones
-vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
-vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+map_key('i', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+map_key('s', '<Tab>', 'v:lua.tab_complete()', { expr = true })
+map_key('i', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
+map_key('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
 
 -- Map compe confirm and complete functions
-vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
+map_key('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
+map_key('i', '<c-space>', 'compe#complete()', { expr = true })
+
+require 'colorizer'.setup {
+  css = { css = true },
+  scss = { css = true },
+  'javascript',
+	'typescriptreact',
+  html = {
+    mode = 'foreground';
+  }
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,33 +3,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
   use {'wbthomason/packer.nvim', opt = true}
 
-  -- ## Syntax Highlight ## --
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'jparise/vim-graphql'
-
-  -- ## Fuzzy File Finder ## --
-  -- use {'junegunn/fzf', run = ':fzf#install()'}
-  -- use 'junegunn/fzf.vim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-  }
-  
-  -- ## Autocompletion ## --
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use {'rodrigore/coc-tailwind-intellisense', run = 'npm install'}
-
-  -- ## Coloscheme, Statusline, BufferBar ## --
-  use 'morhetz/gruvbox'
-  use 'sainnhe/gruvbox-material'
-  use 'kyazdani42/nvim-web-devicons'
-  use {
-        'glepnir/galaxyline.nvim',
-        branch = 'main',
-        config = function() require 'spaceline' end,
-      }
-  -- use 'romgrk/barbar.nvim' 
-
   -- ## Tpope Classics ## --
   use 'tpope/vim-surround'
   use 'tpope/vim-unimpaired'
@@ -38,12 +11,60 @@ return require('packer').startup(function()
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
 
-  use 'psliwka/vim-smoothie'
-  use 'justinmk/vim-sneak'
-  use {'preservim/nerdtree', requires = {'ryanoasis/vim-devicons', 'vwxyutarooo/nerdtree-devicons-syntax'}}
+  -- ## Syntax Highlight & text objects ## --
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+	use 'norcalli/nvim-colorizer.lua'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+
+  -- Add indentation guides even on blank lines
+  use 'lukas-reineke/indent-blankline.nvim'
+  -- Add git related info in the signs columns and popups
+  use { 
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' }
+  }
+
+  -- ## Coloscheme, Statusline, BufferBar ## --
+  use 'morhetz/gruvbox'
+  use {
+    'glepnir/galaxyline.nvim',
+    branch = 'main',
+    config = function() require 'spaceline' end,
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+
+  -- ## Built-in LSP client & Autocompletion/Snippet plugin ## --
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe'
+  use 'L3MON4D3/LuaSnip'
+
+  -- ## Better tmux/nvim interactions ## --
   use 'tmux-plugins/vim-tmux-focus-events'
   use 'tmux-plugins/vim-tmux'
-  use 'thaerkh/vim-indentguides'
+
+  -- ## Preview Markdown files on Browser ## --
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install'}
-  use 'mhinz/vim-signify'
+
+  -- ## Smooth scrolling ## --
+  use 'psliwka/vim-smoothie'
 end)
+
+  -- use 'sainnhe/gruvbox-material'
+  -- use 'jparise/vim-graphql'
+  -- ## Autocompletion ## --
+  -- use {'neoclide/coc.nvim', branch = 'release'}
+  -- use {'rodrigore/coc-tailwind-intellisense', run = 'npm install'}
+  -- use 'thaerkh/vim-indentguides'
+  -- use 'romgrk/barbar.nvim' 
+  -- use 'justinmk/vim-sneak'
+  -- use {'preservim/nerdtree', requires = {'ryanoasis/vim-devicons', 'vwxyutarooo/nerdtree-devicons-syntax'}}
+  -- use 'mhinz/vim-signify'
+
+  -- -- ## Automatic tags management ## --
+  -- use 'ludovicchabant/vim-gutentags'
+
