a43b960eb002d552fe84067068963cef96f2f4ec
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-07-25 20:43:28 -0500

    Refactor init lua to seperate files

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,31 @@
+local map_key = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+map_key('', '<Space>', '<Nop>', { noremap = true, silent = true })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+--Remap for dealing with word wrap
+map_key('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+map_key('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+
+--Add leader shortcuts
+local options = { noremap = true, silent = true }
+map_key('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], options)
+map_key('n', '<leader><Space>', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], options)
+map_key('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], options)
+map_key('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], options)
+map_key('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], options)
+map_key('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], options)
+map_key('n', '<leader>gr', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], options)
+map_key('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], options)
+map_key('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], options)
+
+-- Better pane navigation
+map_key('n', '<C-h>', [[<C-w>h]], options)
+map_key('n', '<C-j>', [[<C-w>j]], options)
+map_key('n', '<C-k>', [[<C-w>k]], options)
+map_key('n', '<C-l>', [[<C-w>l]], options)
+
+-- Y yank until the end of line
+map_key('n', 'Y', 'y$', { noremap = true })
---
+++ nvim/lua/lsp-config.lua
@@ -0,0 +1,49 @@
+local map_buf_key = vim.api.nvim_buf_set_keymap
+
+-- LSP settings
+local nvim_lsp = require 'lspconfig'
+local on_attach = function(_, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { noremap = true, silent = true }
+  map_buf_key(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  map_buf_key(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  map_buf_key(bufnr, 'v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  map_buf_key(bufnr, 'n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  map_buf_key(bufnr, 'n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  map_buf_key(bufnr, 'n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+-- Enable the following language servers
+local servers = { 'rust_analyzer', 'pyright', 'tsserver', 'gopls' }
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- Change diagnostic symbols in gutter
+local signs = { Error = "✗", Warning = "✻", Hint = "﹅", Information = "⦿" }
+
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
