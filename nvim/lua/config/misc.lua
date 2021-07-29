f036850076e55e47cf8d056af11c145955215aeb
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-07-29 12:15:24 -0500

    Move remaining init.lua to seperate config files

---
+++ nvim/lua/config/misc.lua
@@ -0,0 +1,72 @@
+--Map blankline
+vim.g.indent_blankline_char = '┊'
+vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
+vim.g.indent_blankline_char_highlight = 'LineNr'
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_space_char = '•'
+vim.g.indent_blankline_space_char_blankline = ' '
+
+-- Nvim Tree
+vim.g.nvim_tree_auto_open = 1 
+vim.g.nvim_tree_auto_close = 1
+
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { hl = 'GitGutterAdd', text = '✚' },
+    change = { hl = 'GitGutterChange', text = '~' },
+    delete = { hl = 'GitGutterDelete', text = '_' },
+    topdelete = { hl = 'GitGutterDelete', text = '‾' },
+    changedelete = { hl = 'GitGutterChange', text = '~' },
+  },
+}
+
+require 'colorizer'.setup {
+  css = { css = true },
+  scss = { css = true },
+  'javascript',
+	'typescriptreact',
+  html = {
+    mode = 'foreground';
+  }
+}
+
+require 'lualine'.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'gruvbox',
+    component_separators = {'', ''},
+    section_separators = {' ', ' '},
+    disabled_filetypes = {}
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'filename'},
+    lualine_c = {
+      {
+        'diff',
+        colored = true,
+        symbols = {added = '+', modified = '~', removed = '-'}
+      },
+      {
+        'diagnostics',
+        sources = {'nvim_lsp'},
+        symbols = {error = '✘:', warn = '✻:', info = '﹅:'}
+      }
+    },
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {'fugitive'}
+}
---
+++ nvim/lua/config/treesitter.lua
@@ -0,0 +1,20 @@
+-- Treesitter configuration
+-- Parsers must be installed manually via :TSInstall
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "javascript", "typescript", "tsx", "html", "css", "scss", "go" },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+  indent = {
+    enable = true,
+  },
+}
