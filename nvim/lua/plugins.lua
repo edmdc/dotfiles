cdabb97b0a66d18100aae33969687226d6128498
Edwin Montealvo <ed.dev.mon@gmail.com>
2021-07-29 12:15:48 -0500

    Repale galaxyline w/ lualine for easier config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,34 +11,36 @@ return require('packer').startup(function()
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
 
-  -- ## Syntax Highlight & text objects ## --
+  -- ## Syntax Highlight & Formatter ## --
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
 	use 'norcalli/nvim-colorizer.lua'
+  use 'mhartington/formatter.nvim'
 
+  -- ## Explore files ## --
   use {
     'nvim-telescope/telescope.nvim',
     requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
   }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true }
+  }
 
-  -- Add indentation guides even on blank lines
+  -- ## UI/UX Plugins ## --
+  use 'morhetz/gruvbox'
   use 'lukas-reineke/indent-blankline.nvim'
-  -- Add git related info in the signs columns and popups
+  use 'psliwka/vim-smoothie'
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
   use { 
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' }
   }
 
-  -- ## Coloscheme, Statusline, BufferBar ## --
-  use 'morhetz/gruvbox'
-  use {
-    'glepnir/galaxyline.nvim',
-    branch = 'main',
-    config = function() require 'spaceline' end,
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-
-  -- ## Built-in LSP client & Autocompletion/Snippet plugin ## --
+  -- ## Nvim LSP client and copmletion ## --
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/nvim-compe'
   use 'L3MON4D3/LuaSnip'
@@ -51,20 +53,8 @@ return require('packer').startup(function()
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install'}
 
   -- ## Smooth scrolling ## --
-  use 'psliwka/vim-smoothie'
 end)
 
-  -- use 'sainnhe/gruvbox-material'
-  -- use 'jparise/vim-graphql'
-  -- ## Autocompletion ## --
-  -- use {'neoclide/coc.nvim', branch = 'release'}
-  -- use {'rodrigore/coc-tailwind-intellisense', run = 'npm install'}
-  -- use 'thaerkh/vim-indentguides'
-  -- use 'romgrk/barbar.nvim' 
+ -- Removed plugins that may later be useful
   -- use 'justinmk/vim-sneak'
-  -- use {'preservim/nerdtree', requires = {'ryanoasis/vim-devicons', 'vwxyutarooo/nerdtree-devicons-syntax'}}
-  -- use 'mhinz/vim-signify'
-
-  -- -- ## Automatic tags management ## --
-  -- use 'ludovicchabant/vim-gutentags'
-
+  -- use 'romgrk/barbar.nvim' 
